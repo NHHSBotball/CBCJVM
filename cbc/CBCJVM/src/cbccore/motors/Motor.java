2a88425c464dd21ede5aa00f817b5afe0cbab5ec
DrSakuyaPhD <kirbyfan95@gmail.com>
2010-09-24 02:37:49 +0800

    Added some documentation to cbccore.motors.Motor

--- cbc/CBCJVM/src/cbccore/motors/Motor.java
+++ cbc/CBCJVM/src/cbccore/motors/Motor.java
@@ -20,40 +20,88 @@ import cbccore.Device;
 import cbccore.InvalidPortException;
 
 /**
- * Adds a clean API to replace cbccore.low.Camera, and even adds a few minor
+ * Adds a clean API to replace cbccore.low.Motor, and even adds a few minor
  * features & workarounds for known motor controlling issues.
  * 
  * @author Braden McDorman, Benjamin Woodruff
+ * @see cbccore.movement.plugins.motor.Wheel
  * 
  */
 
 public class Motor {
 	private int port = 0;
-	private static cbccore.low.Motor lowMotor = Device.getLowMotorController(); //could be static?
+	private static cbccore.low.Motor lowMotor = Device.getLowMotorController();
 	private long destTime = -1;
 	
+	/**
+	 * Constructs a Motor which is used to power the robot. Motors are
+	 *  high range (meaning they can rotate an arbitrary amount)
+	 *  low torque (meaning they don't have as much movement power)
+	 * 
+	 * @param  port  The port number that the motor is plugged in to
+	 *  
+	 */
 	public Motor(int port) throws InvalidPortException {
 		if(port < 0 || port > 4) throw new InvalidPortException();
 		this.port = port;
 	}
 	
+	/**
+	 * Set motor speed. It's recommended you use a Back-EMF based function 
+	 *  instead. A Back-EMF function is one that reads the voltage return from
+	 *  the motor so you can better determine the speed and increase the power
+	 *  if neccesary.
+	 * 
+	 * @param  motor    Motor number to change percent of (0-3)
+	 * @param  percent  percent % of full (-100 to 100)
+	 * 
+	 * @see #fd
+	 * @see #bk
+	 * @see #mav
+	 */
 	public void motor(int percent) {
 		lowMotor.motor(port, percent);
 	}
-
+	
+	/**
+	 * Clears the position counter of the motor. The position is counted in
+	 *  ticks.
+	 * 
+	 */
 	public int clearPositionCounter() {
 		return lowMotor.clear_motor_position_counter(port);
 	}
 
+	/**
+	 * Moves at a velocity in ticks per second
+	 * 
+	 * @param  velocity  The velocity to move in ticks per second
+	 *  (-1000 to 1000)
+	 * 
+	 */
 	public int moveAtVelocity(int velocity) {
 		return lowMotor.mav(port, velocity);
 	}
 
+	/**
+	 * Moves to an absolute position in ticks
+	 *
+	 * @param  speed    The speed in ticks per second
+	 * @param  goalPos  The goal position to move toward
+	 *
+	 */
 	public int moveToPosition(int speed, int goalPos) {
 		setDestTime(speed, goalPos-getPositionCounter());
 		return lowMotor.mtp(port, speed, goalPos);
 	}
 
+	/**
+	 * Moves to a relative position in ticks
+	 *
+	 * @param  speed    The speed in ticks per second
+	 * @param  goalPos  The goal position to move toward
+	 *
+	 */
 	public int moveRelativePosition(int speed, int deltaPos) {
 		setDestTime(speed, deltaPos);
 		return lowMotor.mrp(port, speed, deltaPos);
@@ -91,30 +139,71 @@ public class Motor {
 		lowMotor.set_pid_gains(port, p, i, d, pd, id, dd);
 	}
 
+	/**
+	 * Actively attempts to keep the motor at the current position
+	 *  (Warning: continues to draw current)
+	 *  Imagine the motors are your legs, <code>freeze()</code> is you
+	 *  attempting to stand in place and resist someone pushing you.
+	 *
+	 * @see #off
+	 *
+	 */
 	public int freeze() {
 		return lowMotor.freeze(port);
 	}
 
-	public int getDone() {
-		return lowMotor.get_motor_done(port);
+	/**
+	 * Checks if the motor is moving toward a goal.
+	 *
+	 * @return  moving  True: The motor is moving toward a goal, False: It isn't
+	 */
+	public boolean getDone() {
+		return lowMotor.get_motor_done(port)!=0;
 	}
 
+    /**
+     * Checks the motor position
+     *
+     * @return  position  The motor position in ticks (to +/-2147483647)
+     */
 	public int getPositionCounter() {
 		return lowMotor.get_motor_position_counter(port);
 	}
 
+    /**
+     * Stops until the motor is done moving to it's goal.
+     */
 	public void blockMotorDone() {
 		lowMotor.bmd(port);
 	}
 
+    /**
+     * <code>motor()</code> only from (+/-255).
+     *
+     * @param  pwm  The the motor power (+/-255)
+     * @see    #motor
+     * @see    #getPwm
+     */
 	public int setPwm(int pwm) {
 		return lowMotor.setpwm(port, pwm);
 	}
 
+    /**
+     * Checks the current motor power.
+     *
+     * @return  pwm  Returns the current pwm setting (+/-255)
+     * @see     #setPwm
+     */
 	public int getPwm() {
 		return lowMotor.getpwm(port);
 	}
 
+    /**
+     * Moves the motor forward at full power. Not a Back-EMF function.
+     *  A Back-EMF fuction such as <code>mav()</code> is recommended.
+     *
+     * @see #mav
+     */
 	public void forward() {
 		lowMotor.fd(port);
 	}
