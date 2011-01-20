5e6b5f9871fe486dcf513e6b199592a2ed5271fe
Brady O'Brien <brady.obrien666@gmail.com>
2011-01-20 17:34:43 -0600

    Added calibration routines to cbccore.sensor.analog.AnalogBooleanAdapter

--- cbc/CBCJVM/src/cbccore/sensors/analog/AnalogBooleanAdapter.java
+++ cbc/CBCJVM/src/cbccore/sensors/analog/AnalogBooleanAdapter.java
@@ -17,6 +17,7 @@
 package cbccore.sensors.analog;
 
 import cbccore.sensors.digital.IBooleanSensor;
+import cbccore.low.Input;
 
 /**
  * Allows you to use an Analog sensor in a boolean (digital) fashion. If a
@@ -25,6 +26,21 @@ import cbccore.sensors.digital.IBooleanSensor;
 public class AnalogBooleanAdapter implements IBooleanSensor {
 	private Analog sensor = null;
 	private int pivot = 0;
+	private boolean reverseCondition = false;
+	/**
+	 * @param  sensor  The analog sensor to watch
+	 * @param  pivot   The point where the boolean value "pivot"s around, if the
+	 *                 value goes below this, false is returned otherwise true
+	 *                 is returned<p/>
+	 *                 If you want to do <code>x >= 50</code> then your pivot
+	 *                 value would be 50. You can use boolean algebra to form
+	 *                 pivot values for other equations.				
+	 * @see cbccore.sensors.analog.Analog
+	 */
+	public AnalogBooleanAdapter(Analog sensor, int pivot) {
+		this.sensor = sensor;
+		this.pivot = pivot;
+	}
 	
 	/**
 	 * @param  sensor  The analog sensor to watch
@@ -33,18 +49,34 @@ public class AnalogBooleanAdapter implements IBooleanSensor {
 	 *                 is returned<p/>
 	 *                 If you want to do <code>x >= 50</code> then your pivot
 	 *                 value would be 50. You can use boolean algebra to form
-	 *                 pivot values for other equations.
+	 *                 pivot values for other equations.	
+	 * @param reverseCondition	if this is set true, than when the sensor input 
+	 *				is greater than the pivot, it will return false, but
+	 *				when it is less than the pivot, it will return true		
 	 * @see cbccore.sensors.analog.Analog
 	 */
-	public AnalogBooleanAdapter(Analog sensor, int pivot) {
+	public AnalogBooleanAdapter(Analog sensor,int pivot,boolean reverseCondition) {
 		this.sensor = sensor;
 		this.pivot = pivot;
+		this.reverseCondition = reverseCondition;	
 	}
-	
 	public Analog getAnalog() {
 		return sensor;
 	}
 	
+	public boolean getReverseCondition() {
+		return reverseCondition	;
+	}
+
+	/**
+	 * @param reverseCondition	if this is set true, than when the sensor input 
+	 *				is greater than the pivot, it will return false, but
+	 *				when it is less than the pivot, it will return true
+	 */				
+	public void setReverseCondition(boolean reverseCondition) {
+		this.reverseCondition = reverseCondition;
+	}
+
 	/**
 	 * The point where the boolean value "pivot"s around, if the value goes
 	 * below this, false is returned otherwise true is returned.
@@ -72,7 +104,43 @@ public class AnalogBooleanAdapter implements IBooleanSensor {
 	
 	@Override
 	public boolean getValue() {
-		if(sensor.getValueHigh() < pivot) return false;
-		return true;
+		if(sensor.getValueHigh() < pivot) return reverseCondition;
+		return !reverseCondition;
+	}
+	
+	/**
+	 * Preforms automatic calibration of sensor based on user input.
+	 */
+	public void calibrateSensor() throws Exception {
+		Input in = new cbccore.low.Input();	//setup input from black_button
+		int threshold = 0;
+
+		System.out.println("Place sensor in true condition and press button");
+		while(in.black_button()==0);
+		while(in.black_button()==1);	//wait for button to be pressed and released
+		int trueval = sensor.getValueHigh();
+	
+		System.out.println("Place sensor in false condition and press button");
+		while(in.black_button()==0);	//wait for button to be pressed and released
+		while(in.black_button()==1);
+		int falseval = sensor.getValueHigh();
+
+		if(trueval>falseval)
+		{
+			threshold=trueval+(2/(falseval-trueval)); //set set threshold to halfway inbetween high
+								// and low ranges
+			reverseCondition=false;			//don't reverse
+		}	
+		if(falseval>trueval)
+		{
+			threshold=falseval+(2/(trueval-falseval));
+			reverseCondition=true;			//reverse true and false
+		}
+		if(falseval==trueval)				//they shouldn't be equal
+			throw new Exception("Bad Calibration! High and low range values are both at "+trueval);
+		pivot=threshold;				//set pivot
+		System.out.println("The high value/range is "+trueval);
+		System.out.println("The low value/range is "+falseval);
+		System.out.println("The threshold value is "+threshold);
 	}
 }
