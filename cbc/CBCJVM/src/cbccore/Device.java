5f20c927100320a7768a472ed4c972120a67807f
John Du Hart <john@compwhizii.net>
2011-05-06 15:22:23 -0400

    Prevents returning low level classes before Device.init() is called

--- cbc/CBCJVM/src/cbccore/Device.java
+++ cbc/CBCJVM/src/cbccore/Device.java
@@ -47,12 +47,21 @@ public class Device {
 	private static Sound lowSound;
 	private static Sensor lowSensors;
 	private static boolean onCBC = true;
+	private static boolean isInited = false;
 	
 	public static void init() {
+		if (isInited) {
+			return;
+		}
+
 		init(new CBCSimulatorFactory());
 	}
 	
 	public static void init(SimulatorFactory preferedSim) {
+		if (isInited) {
+			return;
+		}
+		
 		try {
 			if(System.getProperty("CBC") == null) throw new Exception();
 			System.load("/mnt/user/jvm/cbc/CBC.so");
@@ -109,6 +118,8 @@ public class Device {
 	 * @see     cbccore.low.Motor
 	 */
 	public static Motor getLowMotorController() {
+		init();
+
 		return lowMotors;
 	}
 
@@ -120,6 +131,8 @@ public class Device {
 	 * @see     cbccore.low.Create
 	 */
 	public static Create getLowCreateController() {
+		init();
+
 		return lowCreate;
 	}
 
@@ -131,6 +144,8 @@ public class Device {
 	 * @see     cbccore.low.Display
 	 */
 	public static Display getLowDisplayController() {
+		init();
+
 		return lowDisplay;
 	}
 
@@ -142,6 +157,8 @@ public class Device {
 	 * @see     cbccore.low.Servo
 	 */
 	public static Servo getLowServoController() {
+		init();
+		
 		return lowServos;
 	}
 
@@ -153,6 +170,8 @@ public class Device {
 	 * @see     cbccore.low.Camera
 	 */
 	public static Camera getLowCameraController() {
+		init();
+
 		return lowCamera;
 	}
 
@@ -164,6 +183,8 @@ public class Device {
 	 * @see     cbccore.low.Device
 	 */
 	public static cbccore.low.Device getLowDeviceController() {
+		init();
+
 		return lowDevice;
 	}
 
@@ -175,6 +196,8 @@ public class Device {
 	 * @see     cbccore.low.Input
 	 */
 	public static Input getLowInputController() {
+		init();
+
 		return lowInput;
 	}
 
@@ -186,6 +209,8 @@ public class Device {
 	 * @see     cbccore.low.Sound
 	 */
 	public static Sound getLowSoundController() {
+		init();
+
 		return lowSound;
 	}
 
@@ -197,6 +222,8 @@ public class Device {
 	 * @see     cbccore.low.Sensor
 	 */
 	public static Sensor getLowSensorController() {
+		init();
+
 		return lowSensors;
 	}
 	
